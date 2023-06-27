1ffa05404c93b6c391c40066a547cd08d93f975f
Annyshka <104879302+AnnyshkaShalbi@users.noreply.github.com>
2023-06-27 18:18:35 +0300

    home page

--- frontends/home/display.tsx
+++ frontends/home/display.tsx
@@ -1,11 +1,12 @@
 import { Cemjsx } from "cemjs-all"
-import space from '@svg/space.svg'
+import earn from '@images/logo/earn.png'
 
 export const display = function () {
     return (
         <div class="home home_container">
-            <h2 class="home_title">{this.Static.text}</h2>
-            <img class="home_logoCem" src={space}></img>
+            <h1 class="home_title">{this.Static.title}</h1>
+            <h2 class="home_subtitle">{this.Static.site}</h2>
+            <img class="home_logoCem" src={earn}></img>
         </div>
     )
 
